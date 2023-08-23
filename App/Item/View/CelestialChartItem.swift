ae041f8fd72f229d9950225bc60eec44823f5236
wilks7 <wilks7@icloud.com>
2023-08-22 22:58:54 -0400

    seperated CelestialCharts

---
+++ App/Item/View/CelestialChartItem.swift
@@ -0,0 +1,18 @@
+//
+//  CelestialChartItem.swift
+//  CelestialAbove
+//
+//  Created by Michael Wilkowski on 8/22/23.
+//
+
+import SwiftUI
+
+struct CelestialChartItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CelestialChartItem()
+}
--- Preview/MockData.swift
+++ Preview/MockData.swift
@@ -9,17 +9,17 @@ import SwiftUI
 import WeatherKit
 import CoreLocation
 
-extension PreviewProvider {
-
-    static var timezone: TimeZone { MockData.timezoneLA }
-    static var forecast: [MockData.MockWeather] { MockData.makeForecast(for: .day) }
-    static var event: CelestialEvents { MockData.mars }
-    static var events: [CelestialEvents] { [MockData.saturn, MockData.mars, MockData.jupiter, MockData.venus] }
-}
 class MockData{
     static let locationNY: CLLocation = CLLocation(latitude: 40.7831, longitude: -73.9712)
     static var timezoneNY: TimeZone { TimeZone(identifier: "America/New_York")! }
     static var timezoneLA: TimeZone { TimeZone(identifier: "America/Los_Angeles")! }
     
+    static var forecast: [MockData.MockWeather] { makeForecast(for: .day) }
+
+    
+    static var event: CelestialEvents { MockData.mars }
+    static var events: [CelestialEvents] { MockData.events }
+    
+    
 }
-//
+
